80aa0fca88f186d76f2c124ed1e25c208d1a8008
Carol Xu <carol.xu@emory.edu>
2024-04-09 14:41:43 -0400

    :0

--- analysis/analysis.r
+++ analysis/analysis.r
@@ -15,6 +15,16 @@ ggplot(final.data, aes(x = year, fill = plan_type)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 ggsave("plan_distribution.png")
 
+ggplot(final.data, aes(x = year, y = plan_type, fill = county)) +
+  geom_boxplot() +
+  labs(title = "Distribution of Plan Counts by County Over Time",
+       x = "Year",
+       y = "Number of Plans",
+       fill = "County") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("plan_distribution_2.png")
+
 # 2. bar graphs showing the distribution of star ratings in 2010, 2012, and 2015
 stars = subset(star.ratings, year %in% c(2010, 2012, 2015))
 
@@ -38,14 +48,20 @@ ggplot(avg_payment, aes(x = year, y = avg_ffscost)) +
        x = "Year",
        y = "Average Benchmark Payment") +
   theme_minimal()
-ggsave("avg_payment")
+ggsave("avg_payment.png")
 
 # 4. average share of Medicare Advantage (relative to all Medicare eligibles) over time from 2010 through 2015.
 # which plans do and do not fall under MA? 
 
 ## Estimate ATEs ##
+
 # 5. Running variable
-summary(lm(avg_enrollment~factor(Star_Rating), data=final.data))
+# ran into errors in _BuildFinalData.R, so I am merging final.data and star.ratings here
+merged_data = merge(final.data, star.ratings, by = "contractid")
+
+summary(lm(avg_enrollment~factor(partc_score), data=merged_data))
+
+# 6. Estimate of 3-star, 2.5-star, and 3.5-star ratings on enrollment
 
 
 
